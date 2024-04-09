f2715ff51efddb094da629d92ee92abf4ca25958
Asya <asya_atpulat@hotmail.com>
2024-04-09 22:03:52 +0300

    Initial Commit

---
+++ GetirShoppingApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GetirShoppingApp
+//
+//  Created by Asya Atpulat on 9.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
