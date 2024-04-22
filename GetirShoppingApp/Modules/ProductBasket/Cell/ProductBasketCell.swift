7e3e14962ee69d96a7698b54d35264b2acde2c75
Asya <asya_atpulat@hotmail.com>
2024-04-22 16:51:17 +0300

    Updated fonts and images with extensions in cells.

--- GetirShoppingApp/Modules/ProductBasket/Cell/ProductBasketCell.swift
+++ GetirShoppingApp/Modules/ProductBasket/Cell/ProductBasketCell.swift
@@ -15,69 +15,63 @@ protocol ProductBasketCellDelegate: AnyObject {
 }
 
 class ProductBasketCell: UICollectionViewCell, CustomStepperDelegate {
-    
+
     static let reuseIdentifier = "ProductBasketCell"
     weak var delegate: ProductBasketCellDelegate?
     var product: Product?
-    
-    private lazy var containerView: UIView = {
-        let view = UIView()
-        return view
-    }()
-    
+    private var indexPath: IndexPath?
+
+    private lazy var containerView = UIView()
     private lazy var productImageView = ProductImageView()
-    
+
     private lazy var productNameLabel: UILabel = {
         let label = UILabel()
-        label.font = UIFont(name: "OpenSans-SemiBold", size: 12)
-        label.textColor = UIColor(named: "textDark")
-        label.text = "Product Name"
+        label.font = UIFont.openSansSemiBold(ofSize: 12)
+        label.textColor = UIColor.textDark
         label.numberOfLines = 1
         label.lineBreakMode = .byTruncatingTail
         return label
     }()
-    
+
     private lazy var attributeLabel: UILabel = {
         let label = UILabel()
-        label.font = UIFont(name: "OpenSans-SemiBold", size: 12)
-        label.textColor = UIColor(named: "textSecondary")
-        label.text = "Attribute"
+        label.font = UIFont.openSansSemiBold(ofSize: 12)
+        label.textColor = UIColor.textSecondary
         label.numberOfLines = 2
         label.lineBreakMode = .byTruncatingTail
         return label
     }()
-    
-    lazy var priceLabel: UILabel = {
+
+    private lazy var priceLabel: UILabel = {
         let label = UILabel()
-        label.font = UIFont(name: "OpenSans-Bold", size: 14)
-        label.textColor = UIColor(named: "textPrimary")
-        label.text = "test"
+        label.font = UIFont.openSansSemiBold(ofSize: 14)
+        label.textColor = UIColor.textPrimary
         label.numberOfLines = 2
         label.lineBreakMode = .byTruncatingTail
         return label
     }()
-    
-    lazy var stepper: CustomStepper = {
+
+    private lazy var stepper: CustomStepper = {
         let stepper = CustomStepper(orientation: .horizontal)
         stepper.delegate = self
         return stepper
     }()
-    
+
     private let bottomBorderView: UIView = {
         let view = UIView()
         view.backgroundColor = .bgPrimarySubtle
         return view
     }()
-    
+
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupViews()
     }
-    
+
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     private func setupViews() {
         addSubview(containerView)
         containerView.addSubview(productImageView)
@@ -86,83 +80,98 @@ class ProductBasketCell: UICollectionViewCell, CustomStepperDelegate {
         containerView.addSubview(priceLabel)
         containerView.addSubview(stepper)
         addSubview(bottomBorderView)
-        
+
         containerView.snp.makeConstraints { make in
             make.edges.equalToSuperview()
         }
-        
+
         productImageView.snp.makeConstraints { make in
             make.leading.top.bottom.equalToSuperview()
             make.width.equalTo(74)
         }
-        
+
         productNameLabel.snp.makeConstraints { make in
             make.leading.equalTo(productImageView.snp.trailing).offset(12)
             make.top.equalTo(containerView.snp.top).offset(8.5)
             make.trailing.equalTo(stepper.snp.leading).offset(-12)
         }
-        
+
         attributeLabel.snp.makeConstraints { make in
             make.leading.equalTo(productImageView.snp.trailing).offset(12)
             make.top.equalTo(productNameLabel.snp.bottom).offset(2)
         }
-        
+
         priceLabel.snp.makeConstraints { make in
             make.leading.equalTo(productImageView.snp.trailing).offset(12)
             make.top.equalTo(attributeLabel.snp.bottom).offset(4)
             make.bottom.equalTo(containerView.snp.bottom).offset(-8.5)
         }
-        
+
         stepper.snp.makeConstraints { make in
             make.trailing.equalTo(containerView)
             make.centerY.equalTo(containerView)
             make.width.equalTo(102)
             make.height.equalTo(32)
         }
-        
+
         bottomBorderView.snp.makeConstraints { make in
             make.leading.trailing.equalToSuperview()
             make.height.equalTo(1)
             make.top.equalTo(containerView.snp.bottom).offset(12)
         }
     }
-    
+
     func stepperDidIncrease() {
         guard let product = self.product else { return }
         delegate?.updateProductCounter(for: product, counter: 1)
     }
-    
+
     func stepperDidDecrease() {
         guard let product = self.product else { return }
         delegate?.updateProductCounter(for: product, counter: -1)
     }
-    
+
     func stepperDidReachZero() {
         guard let product = self.product else { return }
         delegate?.updateProductCounter(for: product, counter: -1)
         delegate?.productDidReachZero(product)
     }
-    
-    func configure(with product: Product) {
-        self.product = product
-        priceLabel.text = product.priceText
-        productNameLabel.text = product.name
-        if product.attribute != nil {
-            attributeLabel.text = product.attribute
-        } else {
-            attributeLabel.text = product.shortDescription
-        }
-        if let imageUrl = URL(string: product.imageURL ?? "") {
+
+    private func setImage() {
+        if let imageUrl = URL(string: product?.imageURL ?? "") {
             productImageView.kf.setImage(with: imageUrl)
         }
-        
-        guard let delegate = delegate else { return }
-        let count = delegate.getProductCounter(for: product)
+    }
+
+    private func setAttribute() {
+        if product?.attribute != nil {
+            attributeLabel.text = product?.attribute
+        } else {
+            attributeLabel.text = product?.shortDescription
+        }
+    }
+
+    private func setCount(count: Int) {
         if count > 0 {
             stepper.counterLabel.text = "\(count)"
             stepper.updateMinusButton()
         } else {
             stepperDidReachZero()
         }
+    }
+
+    func updateCounter() {
+        guard let product = product, let delegate = delegate else { return }
+        let count = delegate.getProductCounter(for: product)
+        setCount(count: count)
+    }
+
+    func configure(with product: Product) {
+        self.product = product
+        priceLabel.text = product.priceText
+        productNameLabel.text = product.name
+        setAttribute()
+        setImage()
+        updateCounter()
     }
 }
--- GetirShoppingApp/Modules/ProductListing/Cell/ProductListCell.swift
+++ GetirShoppingApp/Modules/ProductListing/Cell/ProductListCell.swift
@@ -16,50 +16,42 @@ protocol ProductListCellDelegate: AnyObject {
 }
 
 class ProductListCell: UICollectionViewCell, CustomStepperDelegate {
-    
+
     static let reuseIdentifier = "ProductListCell"
-    var product: Product?
-    var presenter: ProductListingPresenterProtocol?
+    private var product: Product?
     weak var delegate: ProductListCellDelegate?
-    
-    private lazy var containerView: UIView = {
-        let view = UIView()
-        return view
-    }()
-    
+
+    private lazy var containerView = UIView()
     private lazy var productImageView = ProductImageView()
-    
-    lazy var priceLabel: UILabel = {
+
+    private lazy var priceLabel: UILabel = {
         let label = UILabel()
-        label.font = UIFont(name: "OpenSans-Bold", size: 14)
-        label.textColor = UIColor(named: "textPrimary")
-        label.text = "test"
+        label.font = UIFont.openSansBold(ofSize: 14)
+        label.textColor = UIColor.textPrimary
         label.numberOfLines = 2
         label.lineBreakMode = .byTruncatingTail
         return label
     }()
-    
+
     private lazy var productNameLabel: UILabel = {
         let label = UILabel()
-        label.font = UIFont(name: "OpenSans-SemiBold", size: 12)
-        label.textColor = UIColor(named: "textDark")
-        label.text = "Product Name"
+        label.font = UIFont.openSansSemiBold(ofSize: 12)
+        label.textColor = UIColor.textDark
         label.numberOfLines = 1
         label.lineBreakMode = .byTruncatingTail
         return label
     }()
-    
+
     private lazy var attributeLabel: UILabel = {
         let label = UILabel()
-        label.font = UIFont(name: "OpenSans-SemiBold", size: 12)
-        label.textColor = UIColor(named: "textSecondary")
-        label.text = "Attribute"
+        label.font = UIFont.openSansSemiBold(ofSize: 12)
+        label.textColor = UIColor.textSecondary
         label.numberOfLines = 2
         label.lineBreakMode = .byTruncatingTail
         return label
     }()
-    
-    lazy var addButton: UIButton = {
+
+    private lazy var addButton: UIButton = {
         let button = UIButton()
         button.setImage(UIImage(named: "plusIcon"), for: .normal)
         button.addTarget(self, action: #selector(addButtonTapped), for: .touchUpInside)
@@ -71,23 +63,23 @@ class ProductListCell: UICollectionViewCell, CustomStepperDelegate {
         button.backgroundColor = UIColor.bgLight
         return button
     }()
-    
+
     private lazy var stepper: CustomStepper = {
         let stepper = CustomStepper(orientation: .vertical)
         stepper.isHidden = true
         stepper.delegate = self
         return stepper
     }()
-    
+
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupViews()
     }
-    
+
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     private func setupViews() {
         stepper.delegate = self
         addSubview(containerView)
@@ -97,71 +89,71 @@ class ProductListCell: UICollectionViewCell, CustomStepperDelegate {
         containerView.addSubview(attributeLabel)
         addSubview(addButton)
         addSubview(stepper)
-        
+
         containerView.snp.makeConstraints { make in
             make.edges.equalToSuperview()
         }
-        
+
         addButton.snp.makeConstraints { make in
             make.width.equalTo(32)
             make.height.equalTo(addButton.snp.width)
             make.top.equalToSuperview().offset(-8)
             make.right.equalToSuperview().inset(-8)
         }
-        
+
         stepper.snp.makeConstraints { make in
             make.width.equalTo(32)
             make.height.equalTo(96)
             make.top.equalToSuperview().offset(-8)
             make.right.equalToSuperview().inset(-8)
         }
-        
+
         priceLabel.snp.makeConstraints { make in
             make.top.equalTo(productImageView.snp.bottom).offset(8)
             make.leading.trailing.equalTo(containerView)
         }
-        
+
         productNameLabel.snp.makeConstraints { make in
             make.top.equalTo(priceLabel.snp.bottom)
             make.leading.trailing.equalTo(containerView)
         }
-        
+
         attributeLabel.snp.makeConstraints { make in
             make.top.equalTo(productNameLabel.snp.bottom).offset(2)
             make.leading.trailing.equalTo(containerView)
         }
-        
+
         productImageView.snp.makeConstraints { make in
             make.width.equalTo(containerView)
             make.height.equalTo(productImageView.snp.width)
         }
     }
-    
+
     @objc private func addButtonTapped() {
+        guard let product = self.product else { return }
         addButton.isHidden = true
         stepper.isHidden = false
-        guard let product = self.product else { return }
         delegate?.addButtonTapped(for: product)
         stepper.counterLabel.text = "1"
         stepper.updateMinusButton()
     }
-    
+
     func stepperDidReachZero() {
         addButton.isHidden = false
         stepper.isHidden = true
     }
-    
+
     func stepperDidIncrease() {
         guard let product = self.product else { return }
         delegate?.updateProductCounter(for: product, counter: 1)
     }
-    
+
     func stepperDidDecrease() {
         guard let product = self.product else { return }
         delegate?.updateProductCounter(for: product, counter: -1)
     }
-    
-    func setCount(count: Int) {
+
+    private func setCount(count: Int) {
         if count > 0 {
             stepper.counterLabel.text = "\(count)"
             stepper.isHidden = false
@@ -172,21 +164,33 @@ class ProductListCell: UICollectionViewCell, CustomStepperDelegate {
             addButton.isHidden = false
         }
     }
-    
+
+    private func setImage() {
+        if let imageUrl = URL(string: product?.imageURL ?? "") {
+            productImageView.kf.setImage(with: imageUrl)
+        }
+    }
+
+    private func setAttribute() {
+        if product?.attribute != nil {
+            attributeLabel.text = product?.attribute
+        } else {
+            attributeLabel.text = product?.shortDescription
+        }
+    }
+
+    private func updateCounter() {
+        guard let product = product, let delegate = delegate else { return }
+        let count = delegate.getProductCounter(for: product)
+        setCount(count: count)
+    }
+
     func configure(with product: Product) {
         self.product = product
         priceLabel.text = product.priceText
         productNameLabel.text = product.name
-        if product.attribute != nil {
-            attributeLabel.text = product.attribute
-        } else {
-            attributeLabel.text = product.shortDescription
-        }
-        if let imageUrl = URL(string: product.imageURL ?? "") {
-            productImageView.kf.setImage(with: imageUrl)
-        }
-        guard let delegate = delegate else { return }
-        let count = delegate.getProductCounter(for: product)
-        setCount(count: count)
+        setImage()
+        setAttribute()
+        updateCounter()
     }
 }
